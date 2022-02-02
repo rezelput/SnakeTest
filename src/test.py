e471dc0a0a6c78eb2175af77d069f73d7bc02eca
Sofiia Orekhova <sofiya_orekhova@outlook.com>
2022-02-02 23:36:19 +0300

    update

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
-pygame.display.set_caption('Snake game by Pythonist')
+pygame.display.set_caption('SnakeTest')
 game_over=False
 while not game_over:
     for event in pygame.event.get():
